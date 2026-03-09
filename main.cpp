1d0b35dec5f60280a26d0c415795f55d88f18dc2
emould2707 <emould@students.avc.edu>
2026-03-09 16:02:04 -0700

    a31 done

--- main.cpp
+++ main.cpp
@@ -8,11 +8,30 @@ int main()
   const double ClassCCost = 9.00;
 
   // TODO: Declare integer variables for tickets sold for each class
+  int ticketsA;
+  int ticketsB;
+  int ticketsC;
 
+  double totalA;
+  double totalB;
+  double totalC;
+  double total;
   // TODO: Declare double variables for total sales per class and grand total
-
+  
+  cout << "Number of tickets sold for class A\n" << endl;
+  cin >> ticketsA;
+  
+  cout << "Number of tickets sold for class B\n" << endl;
+  cin >> ticketsB;
+  
+  cout << "Number of tickets sold for class C\n" << endl;
+  cin >> ticketsC;
   // TODO: Prompt user and read number of Class A tickets sold
 
+  totalA = ticketsA * ClassACost;
+  totalB = ticketsB * ClassBCost;
+  totalC = ticketsC * ClassCCost;
+  total = totalA + totalB + totalC;
   // TODO: Prompt user and read number of Class B tickets sold
 
   // TODO: Prompt user and read number of Class C tickets sold
@@ -26,4 +45,5 @@ int main()
   cout << "Tickets sold for Class B " << ticketsB << " Sales Price for B   " << totalB << endl;
   cout << "Tickets sold for Class C " << ticketsC << " Sales Price for C   " << totalC << endl;
   cout << "Total Sales                              " << total << endl;
+  return 0;
 }
